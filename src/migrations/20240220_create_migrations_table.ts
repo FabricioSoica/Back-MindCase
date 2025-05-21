3a5cd668cb4b7b074a2a124486283e365eb49a37
FabricioSoica <fabriciorosasoica@gmail.com>
2025-05-20 22:41:04 -0300

    FEAT-Criando tabela de gerenciamento de migrations

---
+++ src/migrations/20240220_create_migrations_table.ts
@@ -0,0 +1,25 @@
+import { QueryInterface, DataTypes } from 'sequelize';
+
+export async function up(queryInterface: QueryInterface) {
+  await queryInterface.createTable('Migrations', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    executedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+}
+
+export async function down(queryInterface: QueryInterface) {
+  await queryInterface.dropTable('Migrations');
+} 
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,34 @@
 import { Sequelize } from 'sequelize';
 import sequelize from '../config/database';
+import * as createMigrationsTable from './20240220_create_migrations_table';
 import * as createTables from './20240220_create_tables';
 
 async function runMigrations() {
   try {
-    // Executar migração
-    await createTables.up(sequelize.getQueryInterface());
-    console.log('Migrações executadas com sucesso!');
+    // Primeiro, criar a tabela de migrations se não existir
+    await createMigrationsTable.up(sequelize.getQueryInterface());
+
+    // Verificar quais migrations já foram executadas
+    const [results] = await sequelize.query('SELECT name FROM Migrations');
+    const executedMigrations = results.map((r: any) => r.name);
+
+    // Executar migrations pendentes
+    if (!executedMigrations.includes('20240220_create_tables')) {
+      console.log('Executando migration: create_tables');
+      await createTables.up(sequelize.getQueryInterface());
+      await sequelize.query(
+        'INSERT INTO Migrations (name, executedAt) VALUES (?, NOW())',
+        { replacements: ['20240220_create_tables'] }
+      );
+      console.log('Migration create_tables executada com sucesso!');
+    } else {
+      console.log('Migration create_tables já foi executada anteriormente.');
+    }
+
+    console.log('Todas as migrations foram verificadas!');
     process.exit(0);
   } catch (error) {
-    console.error('Erro ao executar migrações:', error);
+    console.error('Erro ao executar migrations:', error);
     process.exit(1);
   }
 }
